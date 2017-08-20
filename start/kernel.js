5a11e6ec0bccb92f0708251632f42ffdb18478d5
Harminder Virk <virk.officials@gmail.com>
2017-08-20 23:08:11 +0530

    feat(*): initial commit

---
+++ start/kernel.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const Server = use('Server')
+
+/*
+|--------------------------------------------------------------------------
+| Global Middleware
+|--------------------------------------------------------------------------
+|
+| Global middleware are executed on each http request only when the routes
+| match.
+|
+*/
+const globalMiddleware = [
+  'Adonis/Middleware/Cors',
+  'Adonis/Middleware/BodyParser'
+]
+
+/*
+|--------------------------------------------------------------------------
+| Named Middleware
+|--------------------------------------------------------------------------
+|
+| Named middleware is key/value object to conditionally add middleware on
+| specific routes or group of routes.
+|
+| // define
+| {
+|   auth: 'Adonis/Middleware/Auth'
+| }
+|
+| // use
+| Route.get().middleware('auth')
+|
+*/
+const namedMiddleware = {
+  auth: 'Adonis/Middleware/Auth'
+}
+
+Server
+  .registerGlobal(globalMiddleware)
+  .registerNamed(namedMiddleware)
+  .use(['Adonis/Middleware/Static'])
---
+++ start/routes.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/guides/routing
+|
+*/
+
+const Route = use('Route')
+
+Route.get('/', ({ request }) => {
+  return { greeting: 'Hello world in JSON' }
+})
