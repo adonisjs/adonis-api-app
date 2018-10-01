57d31fa23302c8c12cac4751e16cc522de9fcf81
Harminder Virk <virk.officials@gmail.com>
2018-10-01 20:31:11 +0200

    feat(config): add generic cookie options

--- config/app.js
+++ config/app.js
@@ -27,7 +27,7 @@ module.exports = {
   | to encrypted cookies, sessions and other sensitive data.
   |
   */
-  appKey: Env.get('APP_KEY'),
+  appKey: Env.getOrFail('APP_KEY'),
 
   http: {
     /*
@@ -41,7 +41,7 @@ module.exports = {
     | below value to true.
     |
     */
-    allowMethodSpoofing: false,
+    allowMethodSpoofing: true,
 
     /*
     |--------------------------------------------------------------------------
@@ -82,6 +82,7 @@ module.exports = {
     */
     jsonpCallback: 'callback',
 
+
     /*
     |--------------------------------------------------------------------------
     | Etag
@@ -94,6 +95,19 @@ module.exports = {
     |
     */
     etag: false
+  },
+
+  views: {
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Views
+    |--------------------------------------------------------------------------
+    |
+    | Define whether or not to cache the compiled view. Set it to true in
+    | production to optimize view loading time.
+    |
+    */
+    cache: Env.get('CACHE_VIEWS', true)
   },
 
   static: {
@@ -208,5 +222,22 @@ module.exports = {
       filename: 'adonis.log',
       level: 'info'
     }
+  },
+
+  /*
+  |--------------------------------------------------------------------------
+  | Generic Cookie Options
+  |--------------------------------------------------------------------------
+  |
+  | The following cookie options are generic settings used by AdonisJs to create
+  | cookies. However, some parts of the application like `sessions` can have
+  | seperate settings for cookies inside `config/session.js`.
+  |
+  */
+  cookie: {
+    httpOnly: true,
+    sameSite: false,
+    path: '/',
+    maxAge: 7200
   }
 }
