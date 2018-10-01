c34d814c5ed67ad6dbea627964de7d872a9c7f72
Harminder Virk <virk.officials@gmail.com>
2018-10-01 20:32:12 +0200

    feat(middleware): add named guest middleware

--- start/kernel.js
+++ start/kernel.js
@@ -34,7 +34,8 @@ const globalMiddleware = [
 |
 */
 const namedMiddleware = {
-  auth: 'Adonis/Middleware/Auth'
+  auth: 'Adonis/Middleware/Auth',
+  guest: 'Adonis/Middleware/AllowGuestOnly'
 }
 
 /*
