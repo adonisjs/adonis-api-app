639e78e24fb962b14fb3a90ebda860b4dab47a5c
Romain Lanz <romain.lanz@slynova.ch>
2019-01-17 23:25:20 +0100

    feat: add NoTimestamp Trait + ConvertEmptyStringsToNull middleware

--- start/kernel.js
+++ start/kernel.js
@@ -13,7 +13,8 @@ const Server = use('Server')
 |
 */
 const globalMiddleware = [
-  'Adonis/Middleware/BodyParser'
+  'Adonis/Middleware/BodyParser',
+  'App/Middleware/ConvertEmptyStringsToNull'
 ]
 
 /*
